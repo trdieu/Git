8f590a6b45aca0ed1fe72674dc964012ffa57704
Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>
2024-09-26 15:45:17 +0700

    Update StudentManager.java
    Signed-off-by: Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>

--- OOP/Khaibaolopvadoituong/StudentManager.java
+++ OOP/Khaibaolopvadoituong/StudentManager.java
@@ -1,1 +1,9 @@
-students: List<Student>
+public class StudentManager {
+    public List<Student> students;
+
+    public void addStudent(Student student);
+    public void removeStudent(String studentID);
+    public void updateStudent(Student student);
+    public List<Student> listStudents();
+    public List<Student> filterByGPA(double minGPA);
+}
