88e29dab879de5604384fbd0e2f4ebad626008f1
Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>
2024-09-26 15:44:55 +0700

    Update FileManager.java
    Signed-off-by: Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>

--- OOP/Khaibaolopvadoituong/FileManager.java
+++ OOP/Khaibaolopvadoituong/FileManager.java
@@ -1,1 +1,6 @@
+public class FileManager {
+
+    public void saveData(Object data, String fileName) throws IOException;
+    public Object loadData(String fileName) throws IOException, ClassNotFoundException;
+}
 
