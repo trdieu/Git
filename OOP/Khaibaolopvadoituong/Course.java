baa83e00fc36b7cb08c29bf0ebf5778c6570aebc
Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>
2024-09-26 15:42:59 +0700

    Update Course.java
    Signed-off-by: Đinh Đức Mạnh <151888829+DINHDUC-MANH@users.noreply.github.com>

--- OOP/Khaibaolopvadoituong/Course.java
+++ OOP/Khaibaolopvadoituong/Course.java
@@ -1,4 +1,13 @@
-courseID: String
-courseName: String
-credits: int // số tín chỉ
-studentScores: Map<Student, Double>
+public class Course {
+    public String courseID;
+    public String courseName;
+    public int credits;
+    public Map<Student, Double> studentScores;
+
+    
+    public Course(String courseID, String courseName, int credits) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.credits = credits;
+        this.studentScores = new HashMap<>();
+    }
