3b870b401a2df2b9c82301f3031afb7a80b0abad
Diệu <23017208@st.phenikaa-uni.edu.vn>
2025-04-24 10:22:16 +0700

    Initial commit

---
+++ Web/BT/Chat box/script.js
@@ -0,0 +1,9 @@
+function toggleChat() {
+    var chatbox = document.getElementById("chatbox");
+    if (chatbox.style.display === "none" || chatbox.style.display === "") {
+      chatbox.style.display = "block";
+    } else {
+      chatbox.style.display = "none";
+    }
+  }
+  
